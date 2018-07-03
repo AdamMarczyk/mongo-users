e3793b41c7b6983d3a7f1dceaddccfdcf2f798f2
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-03 17:05:17 +0200

    add es6 promise

--- test/create_test.js
+++ test/create_test.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
 const User = require('../src/user');
 
-describe('Creating records', () => {
+describe('Creating records', done => {
   it('saves a user', () => {
     const joe = new User({ name: 'Joe' });
-    joe.save();
+
+    joe.save().then(() => {
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
