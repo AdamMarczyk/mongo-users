e3e8532bfb564911004b1be9f02c34ffa9305c52
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-03 18:45:41 +0200

    add validation function

--- test/validation_test.js
+++ test/validation_test.js
@@ -2,5 +2,18 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
-  it('requires a user name', () => {});
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
+  });
+
+  it('required a user name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === 'Name must be longer than 2 characters.');
+  });
 });
