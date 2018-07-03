dd76ceafec108a940b37b9e666b4502456bcbb6d
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-03 18:11:40 +0200

    increment update test

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -19,5 +19,14 @@ describe('Updating records', () => {
         assert(users[0].name === 'Alex');
         done();
       });
+  });
+
+  it('A user can have their postcount incremented by 1', done => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } }).then(() => {
+      User.findOne({ name: 'Joe' }).then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
+    });
   });
 });
